f2ca4f403fe8f1c7866c7dc8441c488e7a3af61e
Nazar M <nmykhalkevych@ukrinsoft.com>
2019-10-08 09:28:03 +0300

    fix refresh token

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -1,27 +1,22 @@
-import {Injectable} from '@angular/core';
-import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-import {AuthenticationService} from '../services/authentication.service';
-import {Router} from '@angular/router';
-import {AlertService} from '../modules/shared/services/alert.service';
+import { AlertService } from '../modules/shared/services/alert.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService, private alertService: AlertService) {
+  constructor(private alertService: AlertService) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if (err.status === 401) {
-        // auto logout if 401 response returned from api
-        //   this.authenticationService.logout();
-        //   location.reload(true);
+      if (err.status !== 401) {
+        const error = err.error.message || err.statusText;
+        this.alertService.error("Шось пішло не так");
+        return throwError(err);
       }
-      const error = err.error.message || err.statusText;
-      this.alertService.error("Шось пішло не так");
-      return throwError(err);
     }));
   }
 }

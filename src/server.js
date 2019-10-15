37e3ef1a0cc1ae239cfa72819601d2f7b8c89e01
AndriiFytsyk <AndriiFytsyk>
2019-10-15 12:17:55 +0300

    modified server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.static('./dist/social-network'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/social-network/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/social-network/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

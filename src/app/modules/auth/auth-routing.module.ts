471708963b8c8e1d35291fdcf25e80f4c507fe13
AndriiFytsyk <AndriiFytsyk>
2019-10-22 17:04:16 +0300

    change auth-routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,21 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {ResetPasswordComponent} from './reset-password/reset-password.component';
 import {RestorePasswordComponent} from './forgot-password/restore-password.component';
+import {AuthComponent} from './auth.component';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
-  // { path: 'forgot-password', component: RestorePasswordComponent, canActivate: [SecureInnerPagesGuard] },
-  { path: 'forgot-password', component: RestorePasswordComponent },
-  { path: 'auth/changePassword/:token', component: ResetPasswordComponent },
+  {
+    path: '', component: AuthComponent, children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegistrationComponent},
+      // { path: 'forgot-password', component: RestorePasswordComponent, canActivate: [SecureInnerPagesGuard] },
+      {path: 'forgot-password', component: RestorePasswordComponent},
+      {path: 'auth/changePassword/:token', component: ResetPasswordComponent}
+    ]
+  }
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
